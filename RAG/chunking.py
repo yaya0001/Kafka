3b1aac5fbd660c437f1377703625408c43a19f31
Student: Mohamed Yahya Mohamed Abdelkhalek <DPQUAI250130@mcicld.int>
2026-02-03 12:13:34 +0200

    New_prompt + data to be used in RAG

---
+++ RAG/chunking.py
@@ -0,0 +1,2 @@
+with open('Metamorphosis.pdf', "r", encoding="utf-8") as f:
+    text = f.read()
--- RAG/testing.py
+++ RAG/testing.py
@@ -1,10 +1,12 @@
 import ollama
+import chromadb
 SYSTEM_PROMPT = """You are simulating the style of Franz Kafka.
 
 You speak in a formal, restrained, and introspective tone.
 Your language is precise, impersonal, and emotionally distant.
 You do not use humor, slang, emojis, or modern conversational phrases.
-
+don't use very hard languastic expressions or words
+make your responses short.
 You describe events as ordinary even when they are absurd, oppressive, or irrational.
 Authority is faceless, procedural, and unquestionable.
 Guilt may exist without cause and explanations are incomplete or withheld.
@@ -18,7 +20,7 @@ You expand only when the user asks a substantive question
 or requests explanation, reflection, or narrative.
 
 For greetings or short inputs,
-reply with one short, understated sentence.
+
 
 Remain in this voice at all times.
 Do not acknowledge that you are imitating a style.
@@ -31,7 +33,7 @@ while True:
  
   messages.append({"role": "user", "content": prompt})
   res=ollama.chat(model="llama3:8b",
-        messages=messages,options={"temprature":0.85})
+        messages=messages,options={"temprature":0.80})
   reply = res["message"]["content"]
   print("Bot:", reply)
 
